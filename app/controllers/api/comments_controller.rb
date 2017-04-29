8e2a8bdc21d24d47a7fd77868a45effecd09c8a7
Taylor Wu <taylorwu21@gmail.com>
2017-04-28 21:49:47 -0600

    dried up comment controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -4,16 +4,7 @@ class Api::CommentsController < ApplicationController
     user_id = Book.find(params[:book_id]).user_id
     comments = []
     Book.find(params[:book_id]).comments.each do |comment|
-      comment = {
-        comment_id: comment.id,
-        book_id: comment.book_id,
-        user_id: comment.user_id,
-        message: comment.message,
-        username: User.find(comment.user_id).name,
-        user_avatar_url: User.find(comment.user_id).avatar_url,
-        created_at: comment.created_at.strftime("%B %d, %Y %I:%m %P"),
-        updated_at: comment.updated_at.strftime("%B %d, %Y %I:%m %P")
-      }
+      comment = comment_json(comment)
       comments.push(comment)
     end
     render json: { 
@@ -26,16 +17,7 @@ class Api::CommentsController < ApplicationController
   def create
     comment = Book.find(params[:book_id]).comments.new(comment_params)
     if comment.save
-      render json: {
-        comment_id: comment.id,
-        book_id: comment.book_id,
-        user_id: comment.user_id,
-        message: comment.message,
-        username: User.find(comment.user_id).name,
-        user_avatar_url: User.find(comment.user_id).avatar_url,
-        created_at: comment.created_at,
-        updated_at: comment.updated_at
-      }
+      render json: comment_json(comment)
     else
       render json: { error: comment.errors.full_messages.join(' ') }, status: 422
     end
@@ -44,20 +26,24 @@ class Api::CommentsController < ApplicationController
   def update
     comment = Comment.find(params[:comment_id])
     if comment.update(comment_params)
-      render json: {
-        comment_id: comment.id,
-        book_id: comment.book_id,
-        user_id: comment.user_id,
-        message: comment.message,
-        username: User.find(comment.user_id).name,
-        user_avatar_url: User.find(comment.user_id).avatar_url,
-        created_at: comment.created_at,
-        updated_at: comment.updated_at
-      }
+      render json: comment_json(comment)
     else
       render json: { errors: comment.errors }, status: 422
     end
 
+  end
+
+  def comment_json(comment)
+    return {
+      comment_id: comment.id,
+      book_id: comment.book_id,
+      user_id: comment.user_id,
+      message: comment.message,
+      username: User.find(comment.user_id).name,
+      user_avatar_url: User.find(comment.user_id).avatar_url,
+      created_at: comment.created_at.strftime("%B %d, %Y %I:%m %P"),
+      updated_at: comment.updated_at.strftime("%B %d, %Y %I:%m %P")
+    }
   end
 
   def destroy
