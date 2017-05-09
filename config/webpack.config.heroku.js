e4d8bf21b5114a6164af748676503aa355a3c496
Taylor Wu <taylorwu21@gmail.com>
2017-05-09 14:39:50 -0600

    updated heroku webpack file

--- config/webpack.config.heroku.js
+++ config/webpack.config.heroku.js
@@ -14,7 +14,7 @@ var production = process.env.NODE_ENV === 'production';
 var config = {
   entry: {
     // Sources are expected to live in $app_root/webpack
-    'application': './client/application.js'
+    'application': ['./client/application.js', './client/sass/index.scss'],
   },
 
   output: {
@@ -51,6 +51,10 @@ var config = {
           limit: 25000
         }
       },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader","css-loader","sass-loader"]
+      }
     ]
   },
 
